996d190729ccf68250930e863bc03719ef0b1234
ItzTK <tylergordineer@gmail.com>
2025-05-05 14:34:59 -0700

    Initial commit

---
+++ src/main/java/com/itztk/trimsexpandeded/TrimsExpandededClient.java
@@ -0,0 +1,10 @@
+package com.itztk.trimsexpandeded;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrimsExpandededClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
